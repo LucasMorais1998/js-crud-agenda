11520ff3b23dd6a48930cefd7ada3127b331d5b2
LucasMorais1998 <lucas14.morais@gmail.com>
2023-07-22 19:30:23 -0300

    create login register route

--- routes.js
+++ routes.js
@@ -3,16 +3,12 @@ const route = express.Router();
 
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
-const registerController = require('./src/controllers/registerController');
 
 // * Home routes
 route.get('/', homeController.index);
 
 // * Login routes
 route.get('/login/index', loginController.index);
-
-// * Register routes
-route.get('/register/index', registerController.index);
-route.post('/register/create-account', registerController.createAccount);
+route.post('/login/register', loginController.register);
 
 module.exports = route;
