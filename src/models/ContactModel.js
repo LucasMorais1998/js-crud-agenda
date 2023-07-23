da02ba84860ca2706ab5bcabefa640d655806e65
LucasMorais1998 <lucas14.morais@gmail.com>
2023-07-23 15:45:03 -0300

    create contact model

---
+++ src/models/ContactModel.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const ContactSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  lastName: { type: String, required: false, default: '' },
+  email: { type: String, required: false, default: '' },
+  tel: { type: String, required: true, default: '' },
+  createdAt: { type: Date, default: Date.now() },
+});
+
+const ContactModel = mongoose.model('Contact', ContactSchema);
+
+class Contact {
+  constructor(body) {
+    this.body = body;
+    this.errors = [];
+    this.contact = null;
+  }
+
+  async register() {
+    this.validate();
+
+    if (this.errors.length > 0) return;
+
+    this.contact = await ContactModel.create(this.body);
+  }
+
+  validate() {
+    this.cleanUp();
+
+    if (this.body.email && !validator.isEmail(this.body.email))
+      this.errors.push('E-mail inválido.');
+
+    if (!this.body.name) this.errors.push('Nome é um campo obrigatório.');
+
+    if (!this.body.email && !this.body.tel)
+      this.errors.push('É necessário e-mail e/ou telefone no cadastro.');
+  }
+
+  cleanUp() {
+    for (const key in this.body) {
+      if (typeof this.body[key] !== 'string') {
+        this.body[key] = '';
+      }
+    }
+
+    this.body = {
+      name: this.body.name,
+      lastName: this.body.lastName,
+      email: this.body.email,
+      tel: this.body.tel,
+    };
+  }
+}
+
+module.exports = Contact;
